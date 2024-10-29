7c2d70ae13d0b9fcc117dc07341a0f866f2b98b0
Pedro Dias <pdr050203@gmail.com>
2024-10-29 18:17:51 -0300

    feat: add input icon change while on focus

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -9,7 +9,9 @@ import { MagnifyingGlass } from "phosphor-react-native";
 import { THEME } from "../../styles/theme";
 
 export function Home() {
-  const [product, setProduct] = useState('caffe')
+  const [product, setProduct] = useState('caffe');
+  const [focus, setFocus] = useState(false);
+  const [inputValue, setInputValue] = useState('');
 
   return (
     <>
@@ -19,11 +21,16 @@ export function Home() {
         <Text style={styles.title}>Encontre o café perfeito para qualquer hora do dia</Text>
 
         <View style={styles.input}>
-          <MagnifyingGlass color={THEME.COLORS.GREY_400} size={18} />
+          <MagnifyingGlass color={focus ? THEME.COLORS.YELLOW : inputValue.length > 0 ? THEME.COLORS.YELLOW_DARK : THEME.COLORS.GREY_400} size={18} />
           <TextInput 
             placeholder="Pesquisar" 
             placeholderTextColor={THEME.COLORS.GREY_400} 
+            onPressIn={() => setFocus(true)}
+            onKeyPress={() => setFocus(false)}
+            value={inputValue}
+            onChangeText={setInputValue}
             style={{ color: THEME.COLORS.GREY_700}}
+            keyboardType="default"
           />
         </View>
       </View>
