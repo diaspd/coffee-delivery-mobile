08b6b7c19737503ed202a7b58803ffb83d87d641
Pedro Dias <pdr050203@gmail.com>
2024-11-04 18:21:47 -0300

    feat: add image to finsish screen

--- src/screens/Finish/index.tsx
+++ src/screens/Finish/index.tsx
@@ -1,10 +1,14 @@
-import { Text, View, Pressable } from "react-native";
+import { Text, View, Pressable, Image } from "react-native";
+
+import Illustration from '../../assets/Illustration.png'
 
 import { styles } from "./styles";
 
 export function Finish() {
   return (
     <View style={styles.container}>
+      <Image source={Illustration} />
+      
       <Text>Uhu! Pedido confirmado</Text>
       <Text>Agora é só aguardar que logo o café chegará até você!</Text>
 
