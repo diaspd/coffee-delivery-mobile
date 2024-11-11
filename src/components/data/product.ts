26647d784fa45c9a0e9d7f92d077ca80db88b09e
Pedro Dias <pdr050203@gmail.com>
2024-11-11 18:40:53 -0300

    feat: add new data to products

--- src/components/data/product.ts
+++ src/components/data/product.ts
@@ -1,20 +1,110 @@
 export const PRODUCTS = [
   {
     id: '1',
-    tag: 'TRADICIONAL',
-    name: 'Latte',
-    price: '9,00',
+    tag: 'especial',
+    name: 'Irlandês',
+    price: '9,90',
     thumb: require('../../assets/coffees/Image.png'),
     product: require('../../assets/coffees/coffee-big.png'),
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly'
   },
   {
-    id: '2',
-    tag: 'DOCE',
-    name: 'Cappucino',
-    price: '19,00',
+    id: '2', 
+    tag: 'tradicional',
+    name: 'Latte',
+    price: '6,90',
     thumb: require('../../assets/coffees/Image.png'),
     product: require('../../assets/coffees/coffee-big.png'),
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    description: 'Café expresso com o dobro de leite e espuma cremosa'
   },
+  {
+    id: '3',
+    tag: 'doce',
+    name: 'Mocaccino',
+    price: '8,50',
+    thumb: require('../../assets/coffees/Image.png'),
+    product: require('../../assets/coffees/coffee-big.png'),
+    description: 'Café expresso com calda de chocolate, pouco leite e espuma'
+  },
+  {
+    id: '4',
+    tag: 'tradicional',
+    name: 'Expresso Tradicional',
+    price: '4,80',
+    thumb: require('../../assets/coffees/Image.png'),
+    product: require('../../assets/coffees/coffee-big.png'),
+    description: 'O tradicional café feito com água quente e grãos moídos'
+  },
+  {
+    id: '5',
+    tag: 'tradicional',
+    name: 'Expresso Americano',
+    price: '6,20',
+    thumb: require('../../assets/coffees/Image.png'),
+    product: require('../../assets/coffees/coffee-big.png'),
+    description: 'Expresso diluído, menos intenso que o tradicional'
+  },
+  {
+    id: '6',
+    tag: 'tradicional',
+    name: 'Expresso Cremoso',
+    price: '6,30',
+    thumb: require('../../assets/coffees/Image.png'),
+    product: require('../../assets/coffees/coffee-big.png'),
+    description: 'Café expresso tradicional com espuma cremosa'
+  },
+  {
+    id: '7',
+    tag: 'tradicional',
+    name: 'Expresso Gelado',
+    price: '5,50',
+    thumb: require('../../assets/coffees/Image.png'),
+    product: require('../../assets/coffees/coffee-big.png'),
+    description: 'Bebida preparada com café expresso e cubos de gelo'
+  },
+  {
+    id: '8',
+    tag: 'doce',
+    name: 'Capuccino',
+    price: '7,50',
+    thumb: require('../../assets/coffees/Image.png'),
+    product: require('../../assets/coffees/coffee-big.png'),
+    description: 'Bebida com canela feita de doses de café, leite e espuma'
+  },
+  {
+    id: '9',
+    tag: 'doce',
+    name: 'Chocolate Quente',
+    price: '7,70',
+    thumb: require('../../assets/coffees/Image.png'),
+    product: require('../../assets/coffees/coffee-big.png'),
+    description: 'Bebida feita com chocolate dissolvido no leite quente e café'
+  },
+  {
+    id: '10',
+    tag: 'especial',
+    name: 'Cubano',
+    price: '10,20',
+    thumb: require('../../assets/coffees/Image.png'),
+    product: require('../../assets/coffees/coffee-big.png'),
+    description: 'Drink gelado de café expresso com rum, creme de leite e hortelã'
+  },
+  {
+    id: '11',
+    tag: 'especial',
+    name: 'Havaiano',
+    price: '11,20',
+    thumb: require('../../assets/coffees/Image.png'),
+    product: require('../../assets/coffees/coffee-big.png'),
+    description: 'Bebida adocicada preparada com café e leite de coco'
+  },
+  {
+    id: '12',
+    tag: 'especial',
+    name: 'Árabe',
+    price: '11,20',
+    thumb: require('../../assets/coffees/Image.png'),
+    product: require('../../assets/coffees/coffee-big.png'),
+    description: 'Bebida preparada com grãos de café árabe e especiarias'
+  }
 ];
