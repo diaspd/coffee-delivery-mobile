d6c743539a39e9ade69da55fe17514a326008503
Pedro Dias <pdr050203@gmail.com>
2024-11-25 20:17:13 -0300

    style: add badge of items on cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,43 +1,51 @@
 import { Pressable, Text, View } from "react-native";
-
-import { ArrowLeft, MapPin, ShoppingCart } from 'phosphor-react-native'
-
+import { ArrowLeft, MapPin, ShoppingCart } from 'phosphor-react-native';
 import { useNavigation, useRoute } from "@react-navigation/native";
-
-import type { AppRoutesProps } from "../../routes/app.routes";
 
 import { THEME } from "../../styles/theme";
 import { styles } from "./styles";
+import type { AppRoutesProps } from "../../routes/app.routes";
+import { useCart } from "../../hooks/useCart";
 
 interface HeaderProps {
-  hasGoBack?: boolean
-  hasCart?: boolean 
+  hasGoBack?: boolean;
+  hasCart?: boolean;
 }
 
-export function Header({hasGoBack, hasCart}: HeaderProps) {
+export function Header({ hasGoBack, hasCart }: HeaderProps) {
   const navigationStack = useNavigation<AppRoutesProps>();
   const route = useRoute();
+
+  const { cart } = useCart();
+  const totalItems = cart.reduce((total, item) => total + item.quantity, 0); // Soma as quantidades de itens
 
   return (
     <View style={styles.container}>
       {hasGoBack ? (
         <Pressable onPress={() => navigationStack.goBack()}>
-          <ArrowLeft color={route.name === "product" ? THEME.COLORS.WHITE : THEME.COLORS.GREY_100}/>
+          <ArrowLeft
+            color={route.name === "product" ? THEME.COLORS.WHITE : THEME.COLORS.GREY_100}
+          />
         </Pressable>
-      ): (
+      ) : (
         <View style={styles.local}>
           <MapPin color={THEME.COLORS.PURPLE} size={20} weight="fill" />
           <Text style={styles.text}>Porto Alegre, RS</Text>
         </View>
-      )} 
+      )}
 
       {hasCart ? (
-        <Pressable onPress={() => navigationStack.navigate('cart')}>
-          <ShoppingCart color={THEME.COLORS.YELLOW_DARK} size={20} weight="fill" />
+        <Pressable onPress={() => navigationStack.navigate('cart')} style={{ position: 'relative' }}>
+          <ShoppingCart color={THEME.COLORS.YELLOW_DARK} size={24} weight="fill" />
+          {totalItems > 0 && (
+            <View style={styles.badge}>
+              <Text style={styles.badgeText}>{totalItems}</Text>
+            </View>
+          )}
         </Pressable>
-      ): (
+      ) : (
         <Text style={styles.cartText}>Carrinho</Text>
       )}
     </View>
-  )
+  );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -20,6 +20,23 @@ export const styles = StyleSheet.create({
     color: THEME.COLORS.GREY_900,
     fontSize: THEME.FONT_SIZE.TEXT_SM
   },
+  badge: {
+    position: 'absolute',
+    top: -16, 
+    right: -15, 
+    backgroundColor: THEME.COLORS.PURPLE, 
+    borderRadius: 10,
+    width: 20,
+    height: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1,
+  },
+  badgeText: {
+    color: THEME.COLORS.WHITE,
+    fontSize: 12,
+    fontWeight: 'bold',
+  },  
   cartText: {
     color: THEME.COLORS.GREY_200,
     fontSize: THEME.FONT_SIZE.TITLE_SM,
